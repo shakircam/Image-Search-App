d939efe8ced7abca33842acf2700009073eb3f73
shakircam <shakircam@gmail.com>
2021-06-22 18:31:15 +0600

    Image Search App Hilt MVVM

---
+++ app/src/main/java/com/example/imagesearchapp/ui/viewmodel/GalleryViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.imagesearchapp.ui.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
+import com.example.imagesearchapp.repository.UnsplashRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class GalleryViewModel @Inject constructor(
+    private val repository: UnsplashRepository
+) : ViewModel() {
+
+    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+
+    val photos = currentQuery.switchMap { queryString ->
+        repository.getSearchResults(queryString).cachedIn(viewModelScope)
+    }
+
+    fun searchPhotos(query: String) {
+        currentQuery.value = query
+    }
+
+    companion object {
+        private const val DEFAULT_QUERY = "planes"
+    }
+}
